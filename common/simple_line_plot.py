e09e77af4c0c574196532316dbd5b1b2cfb47d3e
mchung-code <tkd.m.chung@gmail.com>
2021-01-09 23:00:51 -0700

    Add doc string

--- common/simple_line_plot.py
+++ common/simple_line_plot.py
@@ -2,6 +2,20 @@ import matplotlib.pyplot as plt
 
 
 def show_line_plot(x_series, title, x_label, y_label, y_series=False, plot_arr=False):
+    """
+    Creates a simple line graph
+
+    Args:
+        x_series: Array-like data structure
+        title: <str> graph title
+        x_label: <str> graph x-axis label
+        y_label: <str> graph y-axis label
+        y_series: Array-like data structure
+        plot_arr: <bool> controls whether the y_series parameter will be used
+
+    Returns: <Matplotlib figure> the plotted figure
+
+    """
     fig = plt.subplots()
 
     if plot_arr:
