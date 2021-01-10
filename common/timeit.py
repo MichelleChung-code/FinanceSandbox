a12d131fb8498d306a179db1f841f4d6dcaee15b
mchung-code <tkd.m.chung@gmail.com>
2021-01-09 23:37:11 -0700

    Update console output

--- common/timeit.py
+++ common/timeit.py
@@ -1,18 +1,14 @@
 import time
 
-# timeit decorator taken from: https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
+# timeit decorator modified from: https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
 
 def timeit(method):
     def timed(*args, **kw):
         ts = time.time()
         result = method(*args, **kw)
         te = time.time()
-        if 'log_time' in kw:
-            name = kw.get('log_name', method.__name__.upper())
-            kw['log_time'][name] = int((te - ts) * 1000)
-        else:
-            print('%r  %2.2f ms' % \
-                  (method.__name__, (te - ts) * 1000))
+
+        print('Time elapsed for %r function:  %2.2f ms' % (method.__name__, (te - ts) * 1000))
         return result
 
     return timed
