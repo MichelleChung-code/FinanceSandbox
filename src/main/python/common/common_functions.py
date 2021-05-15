6df1ffb2181e7ac120a61fcb41142bd42d245c11
mchung-code <tkd.m.chung@gmail.com>
2021-05-14 20:28:57 -0600

    Add storing and loading pickle files, common function

--- src/main/python/common/common_functions.py
+++ src/main/python/common/common_functions.py
@@ -1,6 +1,9 @@
 import numpy as np
 import src.main.python.common.constants as const
 import matplotlib.pyplot as plt
+import pickle
+import os
+from pathlib import Path
 
 
 def log_return(df, col_name):
@@ -79,18 +82,66 @@ def rolling_correlation(df, col_name1, col_name2, window=const.NUM_TRADE_DAYS_PE
     return s1.rolling(window).corr(s2)
 
 
+def write_to_disk(data, output_pkl_path):
+    """
+    Write data to disk as pickle file
+
+    Args:
+        data: to write
+        output_pkl_path: <str> path to write to.  Must be a .pkl file
+
+    """
+
+    # Needs to be a pickle file
+    assert output_pkl_path.endswith('.pkl')
+
+    pkl = open(output_pkl_path, 'wb')
+
+    pickle.dump(data, pkl)
+    pkl.close()
+
+
+def read_from_disk(input_pkl_path):
+    """
+    Loads the data from disk pickle file into the system.
+    NOTE: pickle stores according to first in, first out (FIFO) principle.
+
+    Args:
+        input_pkl_path: <str> path to data pickle file to read from
+
+    Returns: the data that was stored
+
+    """
+    # needs to be a pickle file
+    assert input_pkl_path.endswith('.pkl')
+
+    pkl = open(input_pkl_path, 'rb')
+    data = pickle.load(pkl)
+
+    pkl.close()
+    return data
+
+
 if __name__ == '__main__':
-    from src.main.python.SimpleStockDataPlot import extract_data
+    # from src.main.python.SimpleStockDataPlot import extract_data
+    #
+    # ticker = '^GDAXI'
+    # start_date = '2000-01-01'
+    # end_date = '2014-09-26'
+    # df = extract_data(ticker, start_date, end_date)
+    # df, ret_col_name = log_return(df, const.CLOSE)
+    # df, avg_col_name = rolling_average(df, const.CLOSE)
+    # df, std_col_name = rolling_standard_deviation(df, ret_col_name)
+    #
+    # print(df.tail())
+    #
+    # df[[const.CLOSE, std_col_name, ret_col_name]].plot(subplots=True)
+    # plt.show()
 
-    ticker = '^GDAXI'
-    start_date = '2000-01-01'
-    end_date = '2014-09-26'
-    df = extract_data(ticker, start_date, end_date)
-    df, ret_col_name = log_return(df, const.CLOSE)
-    df, avg_col_name = rolling_average(df, const.CLOSE)
-    df, std_col_name = rolling_standard_deviation(df, ret_col_name)
+    import random
 
-    print(df.tail())
+    data = [random.gauss(1.5, 2) for i in range(1000000)]
+    res_path = os.path.join(str(Path(__file__).parents[1]), 'data', 'data.pkl')
 
-    df[[const.CLOSE, std_col_name, ret_col_name]].plot(subplots=True)
-    plt.show()
+    write_to_disk(data, res_path)
+    print(read_from_disk(res_path))
