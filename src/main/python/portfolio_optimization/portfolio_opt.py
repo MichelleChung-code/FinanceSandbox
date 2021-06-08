fde258470b7a24ec19b3c5ac4f5d5369be4e27e6
mchung-code <tkd.m.chung@gmail.com>
2021-06-07 21:40:21 -0600

    Add base code for factor covariance model

--- src/main/python/portfolio_optimization/portfolio_opt.py
+++ src/main/python/portfolio_optimization/portfolio_opt.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cvxpy as cp
 import pprint
+import scipy.sparse as sp
 
 
 # Atomic functions documentation: https://www.cvxpy.org/tutorial/functions/index.html
 
 class MarkowitzOptimizePortfolio:
-    def __init__(self, num_assets, mu, sigma, constraints: list, gamma=0, lev_limit=1):
+    def __init__(self, num_assets, mu, sigma, constraints: list, gamma=0, lev_limit=1, factor_covariance=False,
+                 **kwargs):
         """
         Run a classical markowitz portfolio optimization
 
@@ -23,11 +25,24 @@ class MarkowitzOptimizePortfolio:
         self.gamma = gamma
         self.constrs_ls = constraints
         self.lev_limit = lev_limit
+        self.factor_covar_model_bool = factor_covariance
+
+        # for factor covariance model
+        if self.factor_covar_model_bool:
+            self.D = kwargs.get('D', False)
+            self.factor_loadings = kwargs.get('F', False)
+            self.m = kwargs.get('num_factors', False)
+            if any(isinstance(x, bool) for x in [self.D, self.factor_loadings, self.m]):
+                raise Exception('Factor Covariance Model selected, but required input parameters are missing')
 
         # quick checks on dimensions
         assert self.gamma >= 0
         assert len(self.mu) == self.n
-        assert self.sigma.shape == (self.n, self.n)
+
+        if self.factor_covar_model_bool:
+            assert self.sigma.shape == (self.m, self.m)
+        else:
+            assert self.sigma.shape == (self.n, self.n)
 
     def get_objective_function(self, portfolio_ret, portfolio_variance):
         """
@@ -42,7 +57,7 @@ class MarkowitzOptimizePortfolio:
         """
         return cp.Maximize(portfolio_ret - self.gamma * portfolio_variance)
 
-    def get_contraints(self, w, constr_ls, lev_lim=1):
+    def get_contraints(self, w, constr_ls, f=False, F=False, lev_lim=1):
         """
         Generate and get the contraints
 
@@ -66,23 +81,71 @@ class MarkowitzOptimizePortfolio:
             missing_items = set(constr_ls) - set(dict_constraints.keys())
             raise NotImplementedError('{} constraints have not been implemented'.format(missing_items))
 
-        return [c for k, c in dict_constraints.items() if k in constr_ls]
+        model_constraints = [c for k, c in dict_constraints.items() if k in constr_ls]
+
+        if self.factor_covar_model_bool:
+            if any([isinstance(x, bool) for x in [f, F]]):
+                raise Exception(
+                    'Factor Exposures CVXPY variable and factor loadings must be provided for factor covariance model')
+            model_constraints = model_constraints + [f == F.T@w]
+
+        return model_constraints
 
     def __call__(self):
         # set up the optimization variables
         # long only portfolio
         w = cp.Variable(self.n)  # weight vector to solve for
+
+        if self.factor_covar_model_bool:
+            f = cp.Variable(self.m)  # factor exposures
+            portfolio_variance = cp.quad_form(f, self.sigma) + cp.quad_form(w, self.D)
+        else:
+            portfolio_variance = cp.quad_form(w, self.sigma)  # cp.quad form is the same as w.T @ self.sigma @ w
+
         portfolio_ret = self.mu.T @ w
-        portfolio_variance = cp.quad_form(w, self.sigma)  # cp.quad form is the same as w.T @ self.sigma @ w
 
         # set up and solve the optimization problem
         obj_func = self.get_objective_function(portfolio_ret, portfolio_variance)
-        constraints = self.get_contraints(w, lev_lim=self.lev_limit, constr_ls=self.constrs_ls)
+
+        if self.factor_covar_model_bool:
+            constraints = self.get_contraints(w, lev_lim=self.lev_limit, constr_ls=self.constrs_ls, f=f,
+                                              F=self.factor_loadings)
+        else:
+            constraints = self.get_contraints(w, lev_lim=self.lev_limit, constr_ls=self.constrs_ls)
 
         problem = cp.Problem(obj_func, constraints)
-        problem.solve()
+        problem.solve(verbose=True)
 
-        return {'w': w.value,
-                'portfolio_ret': portfolio_ret.value,
-                'portfolio_variance': portfolio_variance.value,
-                'status': problem.status}
+        res_dict = {'w': w.value,
+                    'portfolio_ret': portfolio_ret.value,
+                    'portfolio_variance': portfolio_variance.value,
+                    'status': problem.status}
+
+        if self.factor_covar_model_bool:
+            res_dict.update({'factor_exposures': f.value})
+
+        return res_dict
+
+
+if __name__ == '__main__':
+    # factor covariance model
+    n_assets = 3000
+    m_factors = 50
+    np.random.seed(1)
+    mu = np.abs(np.random.randn(n_assets, 1))
+    factor_covar_matrix = np.random.randn(m_factors, m_factors)
+    factor_covar_matrix = factor_covar_matrix.T.dot(factor_covar_matrix)
+    D = sp.diags(np.random.uniform(0, 0.9, size=n_assets))  # idiosyncratic risk
+    factor_loading_matrix = np.random.randn(n_assets, m_factors)
+
+    gamma = 0.1
+    lev_lim = 2
+
+    x = MarkowitzOptimizePortfolio(num_assets=n_assets, num_factors=m_factors, mu=mu, sigma=factor_covar_matrix,
+                                   constraints=['sum_to_one', 'leverage_limit'], D=D, F=factor_loading_matrix,
+                                   factor_covariance=True, gamma=gamma, lev_limit=lev_lim)
+    results_dict = x()
+
+    pprint.pprint(results_dict)
+
+
