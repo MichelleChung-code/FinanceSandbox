b7d6630ce78298a3a4cc96b6e175f807e9cbbb75
mchung-code <tkd.m.chung@gmail.com>
2021-06-21 12:48:02 -0600

    Adding a function for calculating rolling max drawdown

--- src/main/python/common/risk_functions.py
+++ src/main/python/common/risk_functions.py
@@ -1,6 +1,7 @@
 from common.common_functions import get_price_data
 import datetime as dt
 from scipy.stats import norm
+import common.constants as const
 
 
 def volatility(ret_df):
@@ -50,6 +51,32 @@ def gaussian_VaR(ret_df):
     return ret_df.mean() + z_score * ret_df.std()
 
 
+def maximum_drawdowns(df_price_data, window_days=const.NUM_TRADE_DAYS_PER_YR):
+    """
+    Compute rolling maximum drawdowns from daily price data
+
+    Args:
+        df_price_data: <pd.DataFrame> for an individual stock's price history
+        window_days: <int> number of days for rolling calculation, defaults to number of annual trading days
+
+    Returns:
+        <pd.DataFrame> of rolling max drawdowns, <pd.Series> of the minimum of these i.e. the worst rolling max drawdown
+        value
+
+    """
+    # maximum prior peaks in the last window_days number of days per day
+    # allow first 252 days to have an expanding window
+    max_peaks = df_price_data.rolling(window_days, min_periods=1).max()
+
+    # will be negative if current date price is lower than the prev window peak i.e. this is a trough
+    daily_drawdowns = (df_price_data - max_peaks) / max_peaks
+
+    # of these daily values, get the rolling minimum which is the maximum drawdown in the previous window_dats
+    max_drawdowns = daily_drawdowns.rolling(window_days, min_periods=1).min()
+
+    return max_drawdowns, max_drawdowns.min()
+
+
 if __name__ == '__main__':
     ls_assets = ['AAPL', 'NKE', 'GOOGL', 'AMZN']
     df = get_price_data(ls_assets, end_date=dt.datetime.today(), look_back_mths=24).pct_change().fillna(0)
@@ -57,3 +84,6 @@ if __name__ == '__main__':
     print(volatility(df))
     print(semi_deviation(df))
     print(gaussian_VaR(df))
+
+    df_price_data = get_price_data(['AAPL'], end_date=dt.datetime.today(), look_back_mths=48)
+    print(maximum_drawdowns(df_price_data=df_price_data))
