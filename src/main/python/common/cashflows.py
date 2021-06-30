3f8202d23a19a09836e2f57b751f04b38c3adb77
mchung-code <tkd.m.chung@gmail.com>
2021-06-29 21:38:49 -0600

    Note in docstring template

--- src/main/python/common/cashflows.py
+++ src/main/python/common/cashflows.py
@@ -2,6 +2,19 @@ import numpy as np
 
 
 def discounted_expected_uncertain_cashflows(probabilities, uncertain_cashflows, t, rf_rate=0.06):
+    """
+
+    * Note, this does not account for risk
+
+    Args:
+        probabilities:
+        uncertain_cashflows:
+        t:
+        rf_rate:
+
+    Returns:
+
+    """
     return (probabilities.T.dot(uncertain_cashflows)) / (1 + rf_rate) ** t
 
 
