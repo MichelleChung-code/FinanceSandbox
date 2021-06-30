0a165e7d0464c551f6fdc43054f0f2c8016fb8f3
mchung-code <tkd.m.chung@gmail.com>
2021-06-29 21:14:41 -0600

    Add discounted expected cashflows

---
+++ src/main/python/common/cashflows.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def discounted_expected_uncertain_cashflows(probabilities, uncertain_cashflows, t, rf_rate=0.06):
+    return (probabilities.T.dot(uncertain_cashflows)) / (1 + rf_rate) ** t
+
+
+if __name__ == '__main__':
+    print(discounted_expected_uncertain_cashflows(np.array([0.5, 0.5]), np.array([49, 53]), t=1 / 12))
