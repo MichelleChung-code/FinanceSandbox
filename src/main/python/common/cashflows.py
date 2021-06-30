ae7a9749fa8364de953079cb7fb59b968bff6165
mchung-code <tkd.m.chung@gmail.com>
2021-06-29 23:42:36 -0600

    Add doc string

--- src/main/python/common/cashflows.py
+++ src/main/python/common/cashflows.py
@@ -3,17 +3,18 @@ import numpy as np
 
 def discounted_expected_uncertain_cashflows(probabilities, uncertain_cashflows, t, rf_rate=0.06):
     """
+    Present value using weighted average as the expected cashflow
 
     * Note, this does not account for risk
 
     Args:
-        probabilities:
-        uncertain_cashflows:
-        t:
-        rf_rate:
+        probabilities: <np.Array> of cashflow probabilities of cashflows at time t
+        uncertain_cashflows: <np.Array> possible cashflows at time t
+        t: <float> period to discount by
+        rf_rate: <float> annual risk free rate
 
     Returns:
-
+        <float> value of discounted expected cash flows
     """
     return (probabilities.T.dot(uncertain_cashflows)) / (1 + rf_rate) ** t
 
