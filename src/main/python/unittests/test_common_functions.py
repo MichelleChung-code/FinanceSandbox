1b5a3051f2579b282cf4823959dce8508a818e24
mchung-code <tkd.m.chung@gmail.com>
2021-06-30 17:41:01 -0600

    make uncertain cashflows into unittest

--- src/main/python/unittests/test_common_functions.py
+++ src/main/python/unittests/test_common_functions.py
@@ -15,3 +15,17 @@ class CommonFunctionsUnittests(unittest.TestCase):
         IR_3_sq = ls_ic_br[2][0] ** 2 * ls_ic_br[2][1]
 
         self.assertEqual(np.sqrt(sum([IR_1_sq, IR_2_sq, IR_3_sq])), func_res)
+
+    def test_uncertain_cashflows(self):
+        from common.cashflows import discounted_expected_uncertain_cashflows, \
+            discounted_expected_uncertain_cashflows_risk_adjusted
+
+        probabilities, uncertain_cashflows, t = np.array([0.5, 0.5]), np.array([49, 53]), 1 / 12
+        val_multiplers = np.array([1.38, 0.62])
+
+        without_risk = discounted_expected_uncertain_cashflows(probabilities, uncertain_cashflows, t)
+
+        with_risk = discounted_expected_uncertain_cashflows_risk_adjusted(probabilities, uncertain_cashflows,
+                                                                          val_multiplers, t)
+
+        self.assertTrue(np.allclose([50.75295741344933, 49.99663883238616], [without_risk, with_risk]))
