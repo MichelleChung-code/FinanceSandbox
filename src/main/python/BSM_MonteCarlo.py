5a5b068b730735c966408a660c5b3bec921e8a8f
mchung-code <tkd.m.chung@gmail.com>
2021-07-02 11:42:07 -0600

    Add a maturity only method that can handle both call and put options

--- src/main/python/BSM_MonteCarlo.py
+++ src/main/python/BSM_MonteCarlo.py
@@ -1,8 +1,10 @@
 import numpy as np
 from common.timeit import timeit
+from common.math_functions import standard_normal_with_moment_matching
 from black_scholes_merton import BSM_pricing_value
 from common.simple_line_plot import show_line_plot
 import matplotlib.pyplot as plt
+
 
 @timeit
 def BSM_monte_carlo(iter_num, step_num, S, K, r, t, sigma):
@@ -58,3 +60,45 @@ def BSM_monte_carlo(iter_num, step_num, S, K, r, t, sigma):
     # return the monte carlo estimator, C
     return np.exp(-r * t) * (1 / iter_num) * sum(inner_vals_ht)
 
+
+@timeit
+def BSM_monte_carlo_at_maturity_only(iter_num, S, K, r, t, sigma, option_type='call'):
+    """
+    Monte carlo computation of the call or put option price
+
+    At maturity only, no need for storing the whole path
+
+    Args:
+        iter_num: <int> number of iterations of pseudo random numbers
+        S: <float> underlying asset price
+        K: <float> strike price
+        r: <float> annualized risk-free interest rate
+        t: <float> years to maturity
+        sigma: <float> standard deviation of asset returns
+        option_type: <str> can be 'call' or 'put'
+
+    Returns:
+        C: <float> monte carlo estimator for the call option price
+    """
+
+    # get array of random numbers
+    rand_nums = standard_normal_with_moment_matching((2, iter_num))
+
+    # get the S value at maturity for various simulations
+    S_arr = S * np.exp((r - (0.5) * (sigma ** 2)) * t + sigma * (t ** 0.5) * rand_nums[1])
+
+    # get inner values
+    if option_type == 'call':
+        inner_vals_ht = np.maximum(S_arr - K, 0)
+    elif option_type == 'put':
+        inner_vals_ht = np.maximum(K - S_arr, 0)
+    else:
+        raise NotImplementedError
+
+    # return the monte carlo estimator, C
+    return np.exp(-r * t) * (1 / iter_num) * sum(inner_vals_ht)
+
+
+if __name__ == '__main__':
+    print(BSM_monte_carlo_at_maturity_only(50000, 100, 110, 0.05, 1, 0.25, option_type='call'))
+    print(BSM_monte_carlo_at_maturity_only(50000, 100, 110, 0.05, 1, 0.25, option_type='put'))
